4cd284492c0b19a95587ad9fec4c8e6d9f25dc51
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-17 04:05:07 -0500

    Add Card#face and spec

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,17 @@
 class Card
   attr_reader :value, :suit
+  
   def initialize(value, suit)
     @value, @suit = value, suit
+  end
+
+  def face
+    case @value
+    when 1..10 then @value.to_s
+    when 11 then 'J'
+    when 12 then 'Q'
+    when 13 then 'K'
+    when 14 then 'A'
+    end
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require 'card'
 
 describe Card do
-  context "#initialize" do
+  context '#initialize' do
     it 'should initialize with a value and suit' do
       card_1 = Card.new(2, :D)
       expect(card_1.value).to eq(2)
@@ -11,6 +11,16 @@ describe Card do
       card_2 = Card.new(14, :C)
       expect(card_2.value).to eq(14)
       expect(card_2.suit).to eq(:C)
+    end
+  end
+
+  context '#face' do
+    it 'should return the face string for the cards value' do
+      card_1 = Card.new(14, :S)
+      expect(card_1.face).to eq('A')
+      
+      card_2 = Card.new(13, :C)
+      expect(card_2.face).to eq('K')
     end
   end
 end
