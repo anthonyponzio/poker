028e33ec0247765b97a02fec6cc6e59260356ad4
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-18 01:59:50 -0500

    Add Hand#detect_hand, Hand#flush? and Hand#straight?

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,7 +1,46 @@
 class Hand
+
   attr_reader :cards
 
   def initialize(cards)
     @cards = cards
+  end
+
+  def detect_hand
+    return 'straight flush' if straight? && flush?
+
+    count = Hash.new(0)
+
+    
+    cards.each { |card| count[card.value] += 1 }
+    values = count.values.sort
+
+    if values[-1] == 4
+      'four of a kind'
+    elsif values[-1] == 3
+      values[-2] == 2 ? 'full house' : 'three of a kind'
+    elsif flush?
+      'flush'
+    elsif straight?
+      'straight'
+    elsif values[-1] == 2
+      values[-2] == 2 ? 'two pair' : 'one pair'
+    else
+      'high card'
+    end
+  end
+
+  def flush?
+    cards.all? { |card| cards[0].suit == card.suit }
+  end
+
+  def straight?
+    sorted_cards = cards.sort_by { |card| card.value }
+
+    (0..3).to_a.all? do |i|
+      card_1 = sorted_cards[i]
+      card_2 = sorted_cards[i + 1]
+      card_1.value + 1 == card_2.value
+    end
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -4,57 +4,88 @@ require 'card'
 
 describe Hand do
   
-  subject(:hand) { Hand.new(cards) }
-  let(:cards) { [Card.new(1, :D), Card.new(2, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)] }
+  
+  
   
   context '#initialize' do
+    cards = [Card.new(1, :D), Card.new(2, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)]
+    hand = Hand.new(cards)
     it 'should take array of 5 cards and set it to @cards' do
       expect(hand.cards.length).to eq(5)
     end
   end
 
-  context '#detect_hand' do
-    it 'should detect straight flush' do
-      expect(hand.detect_hand).to eq('straight flush')
+  context '#flush?' do
+    it 'should return true when hand is flush' do
+      cards = [Card.new(2, :D), Card.new(3, :D), Card.new(7, :D), Card.new(9, :D), Card.new(11, :D)]
+      hand = Hand.new(cards)
+
+      expect(hand.flush?).to be true      
     end
 
-    let(:cards) { [Card.new(12, :D), Card.new(12, :C), Card.new(12, :H), Card.new(12, :S), Card.new(5, :D)] }
-    it 'should detect four of a kind' do
-      expect(hand.detect_hand).to eq('four of a kind')
-    end
-
-    let(:cards) { [Card.new(1, :D), Card.new(1, :H), Card.new(1, :C), Card.new(4, :D), Card.new(4, :H)] }
-    it 'should detect full house' do
-      expect(hand.detect_hand).to eq('full house')
-    end
-
-    let(:cards) { [Card.new(1, :D), Card.new(2, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)] }
-    it 'should detect flush' do
-      expect(hand.detect_hand).to eq('flush')
-    end
-
-    let(:cards) { [Card.new(1, :D), Card.new(2, :C), Card.new(3, :H), Card.new(4, :D), Card.new(5, :S)] }
-    it 'should detect straight' do
-      expect(hand.detect_hand).to eq('staight')
-    end
-
-    let(:cards) { [Card.new(4, :D), Card.new(2, :D), Card.new(13, :D), Card.new(11, :D), Card.new(9, :D)] }
-    it 'should detect three of a kind' do
-      expect(hand.detect_hand).to eq('three of a kind')
-    end
-
-    let(:cards) { [Card.new(1, :D), Card.new(1, :H), Card.new(3, :C), Card.new(5, :D), Card.new(5, :S)] }
-    it 'should detect two pair' do
-      expect(hand.detect_hand).to eq('two pair')
-    end
-
-    let(:cards) { [Card.new(10, :H), Card.new(12, :S), Card.new(4, :S), Card.new(10, :D), Card.new(5, :C)] }
-    it 'should detect one pair' do
-      expect(hand.detect_hand).to eq('one pair')
+    it 'should return false when hand is not flush' do
+      cards = [Card.new(2, :H), Card.new(3, :D), Card.new(7, :C), Card.new(9, :C), Card.new(11, :D)]
+      hand = Hand.new(cards)
+    
+      expect(hand.flush?).to be false
     end
   end
 
-  # context '#hand_score' do
-  #   it 'should return hash containing score and high_card'
-  # end
+  context '#detect_hand' do
+    it 'should detect straight flush' do
+      cards = [Card.new(1, :D), Card.new(2, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)]
+      hand = Hand.new(cards)
+
+      expect(hand.detect_hand).to eq('straight flush')
+    end
+
+    it 'should detect four of a kind' do
+      cards = [Card.new(12, :D), Card.new(12, :C), Card.new(12, :H), Card.new(12, :S), Card.new(5, :D)]
+      hand = Hand.new(cards)
+      
+      expect(hand.detect_hand).to eq('four of a kind')
+    end
+
+    it 'should detect full house' do
+      cards = [Card.new(1, :D), Card.new(1, :H), Card.new(1, :C), Card.new(4, :D), Card.new(4, :H)]
+      hand = Hand.new(cards)
+
+      expect(hand.detect_hand).to eq('full house')
+    end
+
+    it 'should detect flush' do
+      cards = [Card.new(1, :D), Card.new(8, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)]
+      hand = Hand.new(cards)      
+
+      expect(hand.detect_hand).to eq('flush')
+    end
+
+    it 'should detect straight' do
+      cards = [Card.new(1, :D), Card.new(2, :C), Card.new(3, :H), Card.new(4, :D), Card.new(5, :S)]
+      hand = Hand.new(cards)
+
+      expect(hand.detect_hand).to eq('straight')
+    end
+
+    it 'should detect three of a kind' do
+      cards = [Card.new(4, :D), Card.new(4, :C), Card.new(4, :C), Card.new(11, :D), Card.new(9, :D)]
+      hand = Hand.new(cards)
+      
+      expect(hand.detect_hand).to eq('three of a kind')
+    end
+
+    it 'should detect two pair' do
+      cards = [Card.new(1, :D), Card.new(1, :H), Card.new(3, :C), Card.new(5, :D), Card.new(5, :S)]
+      hand = Hand.new(cards)
+      
+      expect(hand.detect_hand).to eq('two pair')
+    end
+
+    it 'should detect one pair' do
+      cards = [Card.new(10, :H), Card.new(12, :S), Card.new(4, :S), Card.new(10, :D), Card.new(5, :C)]
+      hand = Hand.new(cards)
+
+      expect(hand.detect_hand).to eq('one pair')
+    end
+  end
 end
