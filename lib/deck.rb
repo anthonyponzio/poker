3ee342ee7885ee5fa6a744408d70d82139e92784
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-17 20:45:48 -0500

    Add Deck#initialize

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,2 +1,13 @@
+require_relative 'card'
+
 class Deck
+  SUITS = [:S, :H, :C, :D]
+
+  attr_reader :cards
+  
+  def initialize
+    @cards = SUITS.flat_map do |suit|
+      (1..13).map { |value| Card.new(value, suit) }
+    end
+  end
 end
