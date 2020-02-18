0a89c832bc7f66c374de5a9f5aab1ff889d13c03
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-18 02:23:28 -0500

    Update spec to use symbols for hand identification

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -16,56 +16,63 @@ describe Hand do
       cards = [Card.new(1, :D), Card.new(2, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)]
       hand = Hand.new(cards)
 
-      expect(hand.detect_hand).to eq('straight flush')
+      expect(hand.detect_hand).to eq(:straight_flush)
     end
 
     it 'should detect four of a kind' do
       cards = [Card.new(12, :D), Card.new(12, :C), Card.new(12, :H), Card.new(12, :S), Card.new(5, :D)]
       hand = Hand.new(cards)
       
-      expect(hand.detect_hand).to eq('four of a kind')
+      expect(hand.detect_hand).to eq(:four_of_a_kind)
     end
 
     it 'should detect full house' do
       cards = [Card.new(1, :D), Card.new(1, :H), Card.new(1, :C), Card.new(4, :D), Card.new(4, :H)]
       hand = Hand.new(cards)
 
-      expect(hand.detect_hand).to eq('full house')
+      expect(hand.detect_hand).to eq(:full_house)
     end
 
     it 'should detect flush' do
       cards = [Card.new(1, :D), Card.new(8, :D), Card.new(3, :D), Card.new(4, :D), Card.new(5, :D)]
       hand = Hand.new(cards)      
 
-      expect(hand.detect_hand).to eq('flush')
+      expect(hand.detect_hand).to eq(:flush)
     end
 
     it 'should detect straight' do
       cards = [Card.new(1, :D), Card.new(2, :C), Card.new(3, :H), Card.new(4, :D), Card.new(5, :S)]
       hand = Hand.new(cards)
 
-      expect(hand.detect_hand).to eq('straight')
+      expect(hand.detect_hand).to eq(:straight)
     end
 
     it 'should detect three of a kind' do
       cards = [Card.new(4, :D), Card.new(4, :C), Card.new(4, :C), Card.new(11, :D), Card.new(9, :D)]
       hand = Hand.new(cards)
       
-      expect(hand.detect_hand).to eq('three of a kind')
+      expect(hand.detect_hand).to eq(:three_of_a_kind)
     end
 
     it 'should detect two pair' do
       cards = [Card.new(1, :D), Card.new(1, :H), Card.new(3, :C), Card.new(5, :D), Card.new(5, :S)]
       hand = Hand.new(cards)
       
-      expect(hand.detect_hand).to eq('two pair')
+      expect(hand.detect_hand).to eq(:two_pair)
     end
 
     it 'should detect one pair' do
       cards = [Card.new(10, :H), Card.new(12, :S), Card.new(4, :S), Card.new(10, :D), Card.new(5, :C)]
       hand = Hand.new(cards)
 
-      expect(hand.detect_hand).to eq('one pair')
+      expect(hand.detect_hand).to eq(:one_pair)
+    end
+
+    it 'should detect high card (no other hands)' do
+      cards = [Card.new(10, :H), Card.new(12, :S), Card.new(4, :S), Card.new(1, :D), Card.new(5, :C)]
+      hand = Hand.new(cards)
+
+      expect(hand.detect_hand).to eq(:high_card)
     end
   end
 end
