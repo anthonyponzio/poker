b3cdd34011cabb923a25a16cbf56f700bd6cd32a
anthony ponzio <anthonyponzio1992@gmail.com>
2020-02-17 19:51:05 -0500

    changed ace value to 1 in Card

--- lib/card.rb
+++ lib/card.rb
@@ -7,11 +7,11 @@ class Card
 
   def face
     case @value
-    when 1..10 then @value.to_s
+    when 1 then 'A'
+    when 2..10 then @value.to_s
     when 11 then 'J'
     when 12 then 'Q'
     when 13 then 'K'
-    when 14 then 'A'
     end
   end
 
